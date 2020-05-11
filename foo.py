68af988cfc5605f60edf0eb2e01b2f1f31e6e7cd
ccummings-chris <chris@cummings.tech>
2020-05-10 20:40:45 -0500

    First iteration of splitting a file and converting to IP addresses.

--- foo.py
+++ foo.py
@@ -1,11 +1,39 @@
-import ipaddress
+from bitstring import BitArray
 
 foo = open('input/foo.txt', 'rb')                                                                                                                                       
-data = foo.read()                                                                                                                                                 
-# binary = bin(int.from_bytes(data, byteorder='big'))                                                                                                               
-fml = data[0:16]
-binary = bin(int.from_bytes(fml, byteorder='big'))
-print(binary)
-print(len(binary))
-ip1 = ipaddress.IPv6Address(fml)
-print(ip1)
+data = foo.read()
+binary = BitArray(data)
+
+ 
+global_prefix = '01'
+file_key = '0000000000000100'
+
+i = 0
+index1 = 0
+index2 = 110
+length = len(binary.bin)
+print(binary.bin)
+print('\n')
+print(len(binary.bin))
+print('\n')
+
+while index1 < length:
+    i += 1
+    chunk = binary.bin[index1:index2]
+    print(chunk)
+    print('Iteration: '+ str(i))
+    print('index1')
+    print(index1)
+    print('index2')
+    print(index2)
+    print('\n')
+
+    
+    rendered_IP_bin = global_prefix + file_key + chunk
+    rendered_IP = ipaddress.IPv6Address(int(rendered_IP_bin, 2))
+
+    print(rendered_IP)
+    print('\n')
+
+    index1 += 110
+    index2 += 110
