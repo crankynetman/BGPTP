06d84198dfcf77978aaafb8f1c5fd7addeaa9595
ccummings-chris <chris@cummings.tech>
2020-05-23 11:00:09 -0500

    Added rudimentary chunking

--- foo.py
+++ foo.py
@@ -5,13 +5,14 @@ foo = open('input/foo.txt', 'rb')
 data = foo.read()
 binary = BitArray(data)
 
- 
+padding_value = '0'
+chunk_size = 110
 global_prefix = '01'
 file_key = '0000000000001000'
 
 i = 0
 index1 = 0
-index2 = 110
+index2 = chunk_size
 length = len(binary.bin)
 
 print(data)
@@ -21,9 +22,12 @@ print('\n')
 print(len(binary.bin))
 print('\n')
 
+
 while index1 < length:
     i += 1
+
     chunk = binary.bin[index1:index2]
+    chunk_length = len(chunk)
     # print(chunk)
     # print('Iteration: '+ str(i))
     # print('index1')
@@ -32,9 +36,14 @@ while index1 < length:
     # print(index2)
     # print('\n')
 
+    if chunk_length < 110:
+        padding_size = chunk_size - chunk_length
+        padding_value = padding_value * padding_size
+
+        chunk = str(padding_value) + chunk
     
-    rendered_IP_bin = global_prefix + file_key + chunk
-    rendered_IP = ipaddress.IPv6Address(int(rendered_IP_bin, 2))
+    IP_bin = global_prefix + file_key + chunk
+    rendered_IP = ipaddress.IPv6Address(int(IP_bin, 2))
 
     print(rendered_IP)
     print('\n')
